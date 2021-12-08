a702bf34ce4d530847511fc3fca48ef5a8b02bda
“HLi2000” <lihao20001205@outlook.com>
2021-12-08 03:15:56 +0000

    search

--- src/main/java/Selvet.java
+++ src/main/java/Selvet.java
@@ -84,8 +84,8 @@ public class Selvet extends HttpServlet {
                     psmt.close();
                 }
                 else{
-                    String sql = "SELECT * FROM imgs WHERE Modality IN ('MRI','CT','US','Xray') AND Region IN ('Brain','Chest','Angiogram') AND Patient_name=?";
-                    //String sql = "SELECT * FROM imgs WHERE Patient_name=?";
+                    //String sql = "SELECT * FROM imgs WHERE Modality IN ('MRI','CT','US','Xray') AND Region IN ('Brain','Chest','Angiogram') AND Patient_name=?";
+                    String sql = "SELECT * FROM imgs WHERE Patient_name=?";
                     PreparedStatement psmt = con.prepareStatement(sql);
                     //psmt.setString(1, modality_s);
                     //psmt.setString(1, "'MRI','CT','US','Xray'");
